d5efe5d99da39bcb3294c867a68d2b5a57213a52
amirmammadov <amirmammado@gmail.com>
2023-06-03 19:47:12 +0400

    storybook was installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,28 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true, jest: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-prettier",
-  ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended", "plugin:@typescript-eslint/recommended", "eslint-config-prettier", "plugin:storybook/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  settings: {
+    react: {
+      version: "18.2"
+    }
+  },
   plugins: ["react-refresh"],
   rules: {
-    "no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        args: "after-used",
-        ignoreRestSiblings: true,
-        argsIgnorePattern: "^_",
-      },
-    ],
-    "react/react-in-jsx-scope": "off",
-  },
+    "no-unused-vars": ["error", {
+      vars: "all",
+      args: "after-used",
+      ignoreRestSiblings: true,
+      argsIgnorePattern: "^_"
+    }],
+    "react/react-in-jsx-scope": "off"
+  }
 };
